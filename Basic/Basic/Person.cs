ba5261d6b240b25c21007112600ba16216fc97e5
hanminle <hanml@geohey.com>
2018-07-18 21:05:33 +0800

    有关类中的索引器、访问器、构造函数、析构函数、结构体之间的关系和区别

---
+++ Basic/Basic/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    /// <summary>
+    /// Person pratice
+    /// </summary>
+    class Person
+    {
+        public string name;
+        //静态成员
+        public static Person ps;
+
+        //10个人
+        private string[] psn = new string[4];
+        //表示索引器，可以像访问数组一样访问类中的数组成员
+        public string this[int index]
+        {
+            get
+            {
+                return psn[index];
+            }
+            set
+            {
+                psn[index] = value;
+            }
+        }
+
+
+        //get & set 访问器
+        public string Name
+        {
+            get 
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public Person()
+        {
+            name = "Learning C sharp Hard!";
+        }
+
+        //重载构造函数
+        public Person( string name )
+        {
+            this.name = name;
+        }
+        //私有构造函数只能在类定义时被实例化
+        private Person(string px1,string px2)
+        {
+            Console.WriteLine("Private contructor function would be invoked.");
+            this.name = "Private,"+px1+','+px2+"!";
+        }
+        //静态方法用于返回类的实例
+        public static Person GetPerson()
+        {
+            ps = new Person("Joseph","Simon");
+            return ps;
+        }
+
+        ~Person()
+        {
+            Console.WriteLine("DestroyFunction has been invoked.");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test();
+            Console.Read();
+        }
+
+        static void test()
+        {
+            Person p1 = new Person();
+            Person p2 = new Person("I'M PANJINLIAN.");
+            Person p3 = Person.GetPerson();
+            
+
+            Console.WriteLine(p1.Name);
+            Console.WriteLine(p2.Name);
+            Console.WriteLine(p3.Name);
+
+            Person p4 = new Person();
+            p4[0] = "hello";
+            p4[1] = " ";
+            p4[2] = "world";
+            Console.WriteLine(p4[0]+" "+p4[2]);
+
+        }
+    }
+}
