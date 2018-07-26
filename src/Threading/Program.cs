b58c5986dd0139c0c2a247cbbbe1a443961d823b
hanminle <hanminle@yeah.net>
2018-07-26 21:09:14 +0800

    线程同步与线程锁

---
+++ src/Threading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Tickets.SaleTicketThread1);
+            Thread thread2 = new Thread(Tickets.SaleTicketThread2);
+            thread1.Start();
+            thread2.Start();
+            Console.Read();
+        }
+    }
+}
---
+++ src/Threading/Tickets.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    public class Tickets
+    {
+        static int tickets = 100;
+        static object gobj = new object(); //辅助对象
+        public static void SaleTicketThread1()
+        {
+            while (true)
+            {
+                try
+                {
+                    Monitor.Enter(gobj);
+                    Thread.Sleep(1);
+                    if (tickets > 0)
+                    {
+                        Console.WriteLine("线程1出票:{0}", tickets--);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(gobj);
+                }
+            }
+        }
+        public static void SaleTicketThread2()
+        {
+            while (true)
+            {
+                try
+                {
+                    Monitor.Enter(gobj);
+                    Thread.Sleep(1000);
+                    if (tickets > 0)
+                    {
+                        Console.WriteLine("线程2出票:{0}.", tickets--);
+                    }
+                    else
+                        break;
+                }
+                finally
+                {
+                    Monitor.Exit(gobj);
+                }
+            }
+        }
+    }
+}
