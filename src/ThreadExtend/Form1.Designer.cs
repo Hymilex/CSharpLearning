c9f22e88e8c397ebf9a7141e06eaab56481f3fc3
hanminle <hanminle@yeah.net>
2018-08-09 20:08:08 +0800

    异步操作

---
+++ src/ThreadExtend/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ThreadExtend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.async = new System.Windows.Forms.Button();
+            this.sync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(144, 29);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "测试";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 109);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(207, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // async
+            // 
+            this.async.Location = new System.Drawing.Point(25, 223);
+            this.async.Name = "async";
+            this.async.Size = new System.Drawing.Size(65, 23);
+            this.async.TabIndex = 3;
+            this.async.Text = "asyncBtn";
+            this.async.UseVisualStyleBackColor = true;
+            this.async.Click += new System.EventHandler(this.async_Click);
+            // 
+            // sync
+            // 
+            this.sync.Location = new System.Drawing.Point(128, 223);
+            this.sync.Name = "sync";
+            this.sync.Size = new System.Drawing.Size(65, 23);
+            this.sync.TabIndex = 4;
+            this.sync.Text = "sync";
+            this.sync.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 316);
+            this.Controls.Add(this.sync);
+            this.Controls.Add(this.async);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button async;
+        private System.Windows.Forms.Button sync;
+    }
+}
+
---
+++ src/ThreadExtend/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadExtend
+{
+    public partial class Form1 : Form
+    {
+        //定义委托
+        private delegate void setTextValueCallBack(int value);
+        //声明回调
+        private setTextValueCallBack setCallBack;
+
+
+
+        public void setValue(int val)
+        {
+            this.textBox1.Text = val.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            setCallBack = new setTextValueCallBack(setValue);
+            Thread thread = new Thread(new ThreadStart(Test));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        public  void Test()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                //this.textBox1.Text = i.ToString();
+                
+                this.Invoke(new Action(() => {
+                    setValue(i);
+                }));
+                Thread.Sleep(1000); 
+                //this.Invoke(setCallBack,i);
+            }
+        }
+
+        /// <summary>
+        /// 解决方案一 屏蔽c#对跨线程的检查
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //取消跨线程的访问
+            //Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void async_Click(object sender, EventArgs e)
+        {
+            string str = "***************btnAsync_Click Start:"+Thread.CurrentThread.ManagedThreadId.ToString()+"\n";
+            Action<string> action = this.DoSomethingLong;
+            //传送参数
+            action.Invoke("hello world");
+            
+            action.BeginInvoke("你好啊", null, null);
+            str = str + "***************btnAsync_Click End " + Thread.CurrentThread.ManagedThreadId.ToString();
+            //richTextBox1.Text
+            this.Invoke(new Action(() => {
+                setRichText(str);
+            }));
+
+            //定义回调函数
+            IAsyncResult asyncResult = null;
+            AsyncCallback callback = p => {
+                this.Invoke(new Action(() => {
+                    richTextBox1.Text += "到这里计算已经完成了。" + Thread.CurrentThread.ManagedThreadId.ToString("00").ToString() + "\n";
+                }));
+            };
+            asyncResult = action.BeginInvoke("有回调函数",callback,null);
+
+            Func<string> func = () => {
+                Thread.Sleep(3000);
+                return "这个是不用delegate声明的委托!";
+            };
+
+            IAsyncResult asyncRst = func.BeginInvoke(p => {
+                Console.WriteLine(p.AsyncState);
+                this.Invoke(new Action(() => {
+                    richTextBox1.Text += p.AsyncState.ToString();
+                }));
+            },"异步返回值");
+
+            this.Invoke(new Action(() => {
+                func.EndInvoke(asyncRst);
+            }));
+
+
+        }
+
+
+        public void DoSomethingLong(string name)
+        {
+            string str = "****************DoSomethingLong {" + name + "} Start " + Thread.CurrentThread.ManagedThreadId.ToString("00") + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "***************\n";
+            long lResult = 0;
+            for (int i = 0; i < 1000000000; i++)
+            {
+                lResult += i;
+            }
+            str =str+ "****************DoSomethingLong {" + name + "}  End  " + Thread.CurrentThread.ManagedThreadId.ToString("00") + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "***************\n";
+            this.Invoke(new Action(() =>
+            {
+                this.setRichText(str);
+            }));
+        }
+        void setRichText(string val)
+        {
+            this.richTextBox1.Text += val;
+        }
+    }
+}
